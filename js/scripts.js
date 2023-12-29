c0e81a009d5a3dd79171c13db4b9c15685e34545
RonySatiar <roneeebd@gmail.com>
2023-12-29 19:58:55 +0600

    Commits

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,4 @@
 
-function resizeIframe() {
-  var iframes = document.getElementsByClassName('quarto-iframe');
-
-  for (var i = 0; i < iframes.length; i++) {
-    var iframe = iframes[i];
-    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-    iframe.style.height = (innerDoc.body.scrollHeight + 20) + 'px';
-  }
-}
-
-// Call the resizeIframe function whenever the content inside any iframe with the class 'myIframe' changes
-window.onload = function () {
-  resizeIframe();
-  // You might also want to bind this to other events like resize or content changes in the iframe
-  // For example: iframe.contentWindow.addEventListener('resize', resizeIframe);
-};
 
 
 // Toggle Expand
